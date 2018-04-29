e36d883c0a2e282864cf47d1df4074fcd9f6730e
TFM <momentary@protonmail.com>
2018-04-30 00:46:06 +1000

    Removed .vscode and .eslintrc.js from .gitignore and commited.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "warn",
+            4
+        ],
+        "linebreak-style": [
+            "off",
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+            "off"
+        ]
+    }
+};
