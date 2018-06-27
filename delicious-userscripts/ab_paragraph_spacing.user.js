21d99f28c822dcf5100001942452460eb038dc0d
momentary0 <5304968+momentary0@users.noreply.github.com>
2018-06-27 12:23:40 +1000

    Implemented detecting trailing \n of prior text node.

--- delicious-userscripts/ab_paragraph_spacing.user.js
+++ delicious-userscripts/ab_paragraph_spacing.user.js
@@ -13,16 +13,32 @@
     /**
      * @type {Array<HTMLBRElement>}
      */
-    var brs = document.querySelectorAll('br+br');
+    var brs = Array.from(document.getElementsByTagName('br'));
     for (var i = 0; i < brs.length; i++) {
         var br = brs[i];
-        var prevBr = br.previousElementSibling;
-        var div = document.createElement('div');
-        div.style.marginBottom = '8pt';
+        var prevBr = br.previousSibling;
+        if ((prevBr && prevBr.tagName && prevBr.tagName.toUpperCase() === 'BR' && prevBr.style.display !== 'none')
+            || (prevBr.nodeType === Node.TEXT_NODE && prevBr.nodeValue.slice(-1) === '\n')) {
+            //console.log(prevBr.tagName.toUpperCase());
+            var div = document.createElement('div');
+            div.className = 'paragraph-spacer';
+            div.style.marginBottom = '8pt';
 
-        prevBr.insertAdjacentElement('afterend', div);
-        prevBr.style.display = 'none';
-        br.style.display = 'none';
+            br.parentNode.insertBefore(div, br);
+            //console.log(div);
+            if (prevBr.nodeType === Node.TEXT_NODE) {
+                var spaceLength = /\n*$/.exec(prevBr.nodeValue)[0].length;
+                var newlineTextNode = prevBr.splitText(prevBr.nodeValue.length - spaceLength);
+                newlineTextNode.parentNode.removeChild(newlineTextNode);
+                var newBr = document.createElement('br');
+                newBr.style.display = 'none';
+                br.parentNode.insertBefore(newBr, div);
+            } else {
+                prevBr.style.display = 'none';
+            }
+            br.style.display = 'none';
+        }
+
     }
 
 })();
