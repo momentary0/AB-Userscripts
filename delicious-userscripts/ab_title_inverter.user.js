a277bf3589c8615a7d4675c4df1525900e0e29ff
momentary0 <5304968+momentary0@users.noreply.github.com>
2018-07-07 21:01:36 +1000

    Migrated title inverter.

--- delicious-userscripts/ab_title_inverter.user.js
+++ delicious-userscripts/ab_title_inverter.user.js
@@ -7,12 +7,21 @@
 // @include https://animebytes.tv/forums.php?*
 // @match https://animebytes.tv/forums.php?*
 // @grant none
+// @require https://raw.githubusercontent.com/momentary0/AB-Userscripts/delicious-settings/delicious-library/src/ab_delicious_library.js
 // ==/UserScript==
 
 // Forums title inverter by Potatoe
 // Inverts the forums titles.
 (function ABTitleInverter() {
+    delicious.settings.init('delicioustitleflip', true);
+    if (delicious.settings.ensureSettingsInserted()) {
+        delicious.settings.addScriptCheckbox('delicioustitleflip', 'Title inverter',
+            'Flips the tab title.');
+    }
+    if (!delicious.settings.get('delicioustitleflip'))
+        return;
+
     if (document.title.indexOf(' > ') !== -1) {
-    document.title = document.title.split(" :: ")[0].split(" > ").reverse().join(" < ") + " :: AnimeBytes";
+        document.title = document.title.split(" :: ")[0].split(" > ").reverse().join(" < ") + " :: AnimeBytes";
     }
 })();
