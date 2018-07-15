856bf3754f108829d2b530708ecc377309589d12
momentary0 <5304968+momentary0@users.noreply.github.com>
2018-07-15 20:43:17 +1000

    Updated create_dev_userscript.py to create an 'automatic' version.

--- delicious-userscripts/create_dev_userscript.py
+++ delicious-userscripts/create_dev_userscript.py
@@ -21,3 +21,24 @@ if __name__ == '__main__':
 (function() {
     console.info('Delicious bundle developer version loaded.');
 })();''')
+
+        with open('./dist/ab_delicious_scripts.auto.user.js', 'w', encoding='utf-8') as f:
+            f.write('''// ==UserScript==
+// @name AnimeBytes Delicious Bundle (Automatic)
+// @version 1.0.0
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_deleteValue
+// @match https://*.animebytes.tv/*
+// @icon http://animebytes.tv/favicon.ico
+// @require https://github.com/momentary0/AB-Userscripts/raw/master/delicious-library/src/ab_delicious_library.js\n''')
+            requires = []
+            requires.extend(glob.glob('./src/*.user.js'))
+            for x in requires:
+                prefix = 'https://github.com/momentary0/AB-Userscripts/raw/delicious-settings/delicious-userscripts'
+                f.write('// @require '+prefix+x[1:].replace('\\', '/') + '\n')
+            f.write('''// ==/UserScript==
+
+(function() {
+    console.info('Delicious bundle automatic version loaded.');
+})();''')
