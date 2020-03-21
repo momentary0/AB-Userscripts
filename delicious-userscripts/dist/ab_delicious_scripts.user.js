d40f0d9e6ec132fcff0038db9c487f5a1aabc4ad
momentary0 <5304968+momentary0@users.noreply.github.com>
2020-03-21 12:51:08 +1000

    Typo.

--- delicious-userscripts/dist/ab_delicious_scripts.user.js
+++ delicious-userscripts/dist/ab_delicious_scripts.user.js
@@ -385,7 +385,7 @@
             // Add required time to size_cell and blockquote in torrent_row
             if (size_index !== null && show_required_time) {
                 var seeding_time = Math.max(0, size - 10) * 5 + 72;
-                seeding_time = Math.min(21*24, seeding_time); // seeding time is called at 21 days.
+                seeding_time = Math.min(21*24, seeding_time); // seeding time is capped at 21 days.
                 size_cell.title = 'You need to seed this torrent for at least\n' + duration_to_string(seeding_time) + '\nor it will become a hit and run!';
                 if (torrent_row !== null) {
                     var block_quote = torrent_row.querySelector('blockquote');
